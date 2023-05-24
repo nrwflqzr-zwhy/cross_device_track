25bb1783c842ab4b8172d6eeaac77ddd346ac324
zwhy <wa22201149@stu.ahu.edu.cn>
2023-05-24 09:25:09 +0800

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zwhy/workspace/cross_device_tracking/src
+# Build directory: /home/zwhy/workspace/cross_device_tracking/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zwhy/workspace/cross_device_tracking/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/zwhy/workspace/cross_device_tracking/build'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages:/home/zwhy/projects/AB3DMOT-master:/home/zwhy/projects/AB3DMOT-master/Xinshuo_PyToolbox'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/zwhy/workspace/cross_device_tracking/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/zwhy/workspace/cross_device_tracking/src:$ROS_PACKAGE_PATH"
